692b5531aa949615244dff07e1d0d2287514fb67
TRJackson998 <85368064+TRJackson998@users.noreply.github.com>
2025-01-26 07:07:45 -0800

    Beginning of Buttons

--- breakout/screens.py
+++ breakout/screens.py
@@ -26,9 +26,13 @@ Created
 """
 
 from dataclasses import dataclass
+from typing import Literal
 
+import pygame
 from pygame.color import Color
 from pygame.surface import Surface
+
+from breakout import screen_size
 
 
 class _Screen:
@@ -49,6 +53,48 @@ class _Screen:
         pygame_window.fill(Color("black"))  # clear screen
         for element in self.elements:
             element.draw(pygame_window)
+
+
+class _Button:
+    """
+    A class to create Button objects, private to screens.py
+
+    Buttons have text, run an 'on_click' function, and change color when a mouse hovers over them
+    Position is used to determine where to place the button. There will be two buttons
+    on each screen, so buttons can be in two positions: top or bottom.
+    """
+
+    def __init__(
+        self,
+        text: str,
+        on_click,
+        position: Literal["top", "bottom"],
+        color: Color = Color("blue"),
+        hover_color: Color = Color("gray"),
+    ):
+        self.text = text
+        self.on_click = on_click
+        self.color = color
+        self.hover_color = hover_color
+
+        # dynamically determine button size based on screen size
+        width: int = screen_size.width // 5
+        height: int = screen_size.height // 15
+        pad: int = screen_size.width // 100
+
+        # dynamically place in middle of the screen
+        x = (screen_size.width // 2) - (width // 2)
+
+        # if it's the top or bottom button
+        if position == "top":
+            # two button's worth up from the bottom of the screen
+            y = screen_size.height - ((height + pad) * 2)
+        else:
+            # one button's worth up from the bottom of the screen
+            y = screen_size.height - height - pad
+
+        # make the rectangle
+        self.rect = pygame.Rect(x, y, width, height)
 
 
 @dataclass
